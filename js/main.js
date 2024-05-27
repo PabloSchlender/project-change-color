c64a7afa4ec029242c87fa7c70eccc037f6c5bc5
Schlender <pablitoschlender01@gmail.com>
2024-05-27 20:43:56 -0300

    primer proyecto

---
+++ js/main.js
@@ -0,0 +1,49 @@
+const changeColorfdc437=()=>{
+    document.getElementById("lorem").style.color="#fdc437";
+}
+document.getElementById("colorfdc437").addEventListener("click",changeColorfdc437);
+
+const changeColor01baef=()=>{
+    document.getElementById("lorem").style.color="#01baef";
+}
+document.getElementById("color01baef").addEventListener("click",changeColor01baef);
+
+const changeColor0cbaba=()=>{
+    document.getElementById("lorem").style.color="#0cbaba";
+}
+document.getElementById("color0cbaba").addEventListener("click",changeColor0cbaba);
+
+const changeColor380036=()=>{
+    document.getElementById("lorem").style.color="#380036";
+}
+document.getElementById("color380036").addEventListener("click",changeColor380036);
+
+const changeColor5da9e9=()=>{
+    document.getElementById("lorem").style.color="#5da9e9";
+}
+document.getElementById("color5da9e9").addEventListener("click",changeColor5da9e9);
+
+const changeColor4a9ccf=()=>{
+    document.getElementById("lorem").style.color="#4a9ccf";
+}
+document.getElementById("color4a9ccf").addEventListener("click",changeColor4a9ccf);
+
+const changeColor661f45=()=>{
+    document.getElementById("lorem").style.color="#661f45";
+}
+document.getElementById("color661f45").addEventListener("click",changeColor661f45);
+
+const changeColorf43e3d=()=>{
+    document.getElementById("lorem").style.color="#f43e3d";
+}
+document.getElementById("colorf43e3d").addEventListener("click",changeColorf43e3d);
+
+const changeColor61e786=()=>{
+    document.getElementById("lorem").style.color="#61e786";
+}
+document.getElementById("color61e786").addEventListener("click",changeColor61e786);
+
+const changeColor0c6834=()=>{
+    document.getElementById("lorem").style.color="#0c6834";
+}
+document.getElementById("color0c6834").addEventListener("click",changeColor0c6834);
